325c9820faa47799805c1dff787a68dfc76ecbf0
SNU\paryoja <paryoja@hanmail.net>
2021-01-25 18:57:51 +0900

    add styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const Theme = {
+  colors: {
+    background: 'radial-gradient(#282c34cc, #282cc3)',
+    black: '#282c34',
+    blue: '#a0e9fd',
+    lightBlue: '#caf3fe',
+    white: 'white',
+  },
+  trasition: '0.3s',
+}
+
+export default Theme
